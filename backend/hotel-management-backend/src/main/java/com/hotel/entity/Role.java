b7046e825d76cb983ea7eecd49f8c3f339380853
Shaik Salam <salam24shaik@gmail.com>
2026-07-13 21:16:05 +0530

    first

---
+++ backend/hotel-management-backend/src/main/java/com/hotel/entity/Role.java
@@ -0,0 +1,31 @@
+package com.hotel.entity;
+
+import com.hotel.enums.RoleName;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Table(name = "roles")
+@Setter
+@Getter
+@NoArgsConstructor
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(length = 30, nullable = false, unique = true)
+	private RoleName name;
+	
+}
